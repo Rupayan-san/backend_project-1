9111ef019bf7152b4532329caa2fbcfffc629efe
Rupayan <rikdasrik297@gmail.com>
2026-04-22 21:09:28 +0530

    added user and video models

--- src/db/db.js
+++ src/db/db.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
 
-const connectDB = async () => {
+const connectDB = async () => { //we r using async here because mongoose.connect returns a promise which takes time
     try {
         // const connectionInstance = await mongoose.connect(process.env.MongoDB_URL)
-        const connectionInstance = await mongoose.connect(process.env.MongoDB_URL, {
-            tls: true,
+        const connectionInstance = await mongoose.connect(process.env.MongoDB_URL, { //wait here until the connection is done
+            tls: true, //Use a secure/encrypted connection
             tlsInsecure: true,   // bypasses cert validation
-            serverSelectionTimeoutMS: 5000
+            serverSelectionTimeoutMS: 5000 //If no connection in 5 seconds, give up
         })
         console.log(`\nMongoDB connected !! DB host : ${connectionInstance.connection.host}`);
     } catch (error) {
---
+++ src/models/user.model.js
@@ -0,0 +1,92 @@
+import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+const userSchema = new Schema(
+    {
+        userName: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+            lowercase: true,
+            index: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+            lowercase: true,
+        },
+        fullName: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+        },
+        avatar: {
+            type: String, // because the photos will be stored in cloud and will be delivered in string format
+            required: true
+        },
+        coverImage: {
+            type: String,
+            required: true
+        },
+        watchHistory: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "video"
+            }
+        ],
+        password: {
+            type: String,
+            required: [true, 'passowrd is required']
+        },
+        refreshToken: {
+            type: String
+        }
+
+    }, { timestamps: true }
+)
+
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next(); //checks if the password is already encrypted cuz we dont want to encrypt repeatedly
+
+    this.password = bcrypt.hash(this.password, 10) // encrypts the password
+    next()
+})
+
+userSchema.methods.isPasswordCorrect = async function (password) { // making a custom method to check if the password sent by user matches our encrypted password
+    return await bcrypt.compare(passowrd, this.password)
+}
+
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            userName: this.userName,
+            email: this.email,
+            fullName: this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+ 
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+} 
+
+
+export const User = mongoose.model("User", userSchema)
