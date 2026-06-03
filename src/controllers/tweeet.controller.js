e174c05b71dab487ebb29cd108eeda18e30c9f3e
Rupayan <rikdasrik297@gmail.com>
2026-06-04 00:13:30 +0530

    added updateTweet endpoint to modify existing tweets with validation

--- src/controllers/tweeet.controller.js
+++ src/controllers/tweeet.controller.js
@@ -63,6 +63,37 @@ const getUserTweets = asyncHandler(async (req, res) => {
 
 const updateTweet = asyncHandler(async (req, res) => {
     //TODO: update tweet
+    const ownerId = req.user?._id
+    const {tweetId} = req.params
+    const {newContent} = req.body
+
+    if (!ownerId || !isValidObjectId(ownerId)) {
+        throw new ApiError(400, "invalid owner id")
+    }
+
+    if (!tweetId || !isValidObjectId(tweetId)) {
+        throw new ApiError(400, "invalid tweet id")
+    }
+
+    if (!newContent || !newContent.trim()) {
+        throw new ApiError(400, "content cannot be empty")
+    }
+
+    if (newContent.length > 100) {
+        throw new ApiError(400, "content length should be within 100")
+    }
+
+    const updatedContent = await Tweet.findByIdAndUpdate(tweetId,
+        {
+            $set: {
+                content: newContent
+            }
+        }, {new: true}
+    )
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, updatedContent, "content updated successfully"))
 })
 
 const deleteTweet = asyncHandler(async (req, res) => {
--- src/routes/tweet.router.js
+++ src/routes/tweet.router.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createTweet, getUserTweets } from "../controllers/tweeet.controller.js"
+import { createTweet, getUserTweets, updateTweet } from "../controllers/tweeet.controller.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router()
@@ -7,5 +7,7 @@ const router = Router()
 router.route("/create-tweet").post(verifyJWT, createTweet)
 
 router.route("/get-user-tweets").get(verifyJWT, getUserTweets)
+
+router.route("/update-tweet/:tweetId").patch(verifyJWT, updateTweet)
 
 export default router
