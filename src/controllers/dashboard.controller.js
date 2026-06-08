f9cfeae56bc19f41b6bee19f6e9939204bb9c1cc
Rupayan <rikdasrik297@gmail.com>
2026-06-08 21:05:45 +0530

    added getChannelVideos endpoint with user validation to fetch channel videos

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -71,6 +71,23 @@ const getChannelStats = asyncHandler(async (req, res) => {
 
 const getChannelVideos = asyncHandler(async (req, res) => {
     // TODO: Get all the videos uploaded by the channel
+    const userId = req.user?._id
+
+    if (!userId || !isValidObjectId(userId)) {
+        throw new ApiError(400, "invalid user id")
+    }
+
+    const videos = await Video.find({
+        owner: new mongoose.Types.ObjectId(userId)
+    })
+
+    if (!videos) {
+        throw new ApiError(404, "no videos")
+    }
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, videos, "videos fetched successfully"))
 })
 
 export {
--- src/routes/dashboard.router.js
+++ src/routes/dashboard.router.js
@@ -1,10 +1,12 @@
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import { Router } from "express";
-import { getChannelStats } from "../controllers/dashboard.controller.js"
+import { getChannelStats, getChannelVideos } from "../controllers/dashboard.controller.js"
 
 const router = Router()
 
 router.route("/getChannelStats").get(verifyJWT, getChannelStats)
+
+router.route("/getChannelVideos").get(verifyJWT, getChannelVideos)
 
 
 export default router
